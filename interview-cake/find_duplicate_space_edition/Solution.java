7c65f82bf506c20403fde2ed617481583ea464e6
mardiniii <sezama03@hotmail.com>
2019-10-06 19:42:21 -0400

    Interview Cake: Add a Java solution to the Find Duplicate Space Edition problem.

---
+++ interview-cake/find_duplicate_space_edition/Solution.java
@@ -0,0 +1,88 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    public static int findRepeat(int[] numbers) {
+        int floor = 1;
+        int ceiling = numbers.length - 1; // One item is repeated, so the options are 1..n
+
+        while (floor < ceiling) {
+            // Create lower and upper ranges
+            int midpoint          = floor + ((ceiling - floor) / 2);
+            int lowerRangeFloor   = floor;
+            int lowerRangeCeiling = midpoint;
+            int upperRangeFloor   = midpoint + 1;
+            int upperRangeCeiling = ceiling;
+
+            // Count number of items in lower range
+            int itemsInLowerRange = 0;
+            for (int item : numbers) {
+                if (item >= lowerRangeFloor && item <= lowerRangeCeiling) {
+                    itemsInLowerRange += 1;
+                }
+            }
+
+            int distinctPossibleIntegersInLowerRange = lowerRangeCeiling - lowerRangeFloor + 1;
+
+            if (distinctPossibleIntegersInLowerRange < itemsInLowerRange) {
+                floor = lowerRangeFloor;
+                ceiling = lowerRangeCeiling;
+            } else {
+                floor = upperRangeFloor;
+                ceiling = upperRangeCeiling;
+            }
+        }
+
+        return floor;
+    }
+
+
+    // tests
+
+    @Test
+    public void justTheRepeatedNumberTest() {
+        final int[] numbers = {1, 1};
+        final int expected = 1;
+        final int actual = findRepeat(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shortArrayTest() {
+        final int[] numbers = {1, 2, 3, 2};
+        final int expected = 2;
+        final int actual = findRepeat(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mediumArrayTest() {
+        final int[] numbers = {1, 2, 5, 5, 5, 5};
+        final int expected = 5;
+        final int actual = findRepeat(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void longArrayTest() {
+        final int[] numbers = {4, 1, 4, 8, 3, 2, 7, 6, 5};
+        final int expected = 4;
+        final int actual = findRepeat(numbers);
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
